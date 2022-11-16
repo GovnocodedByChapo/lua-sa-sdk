62ebe2b733cccd7f5b6995d36243af44817536af
Дима <82971106+GovnocodedByChapo@users.noreply.github.com>
2022-11-16 16:53:38 +0300

    Update CRadar.lua

--- lua_sdk/CRadar.lua
+++ lua_sdk/CRadar.lua
@@ -1,13 +1,15 @@
 local ffi = require('ffi')
 require('lua_sdk.CVector2D')
-local functions = {
+return require('lua_sdk.loader')({
     ['LoadTextures'] = { 'void(__cdecl*)()', 0x5827D0 },
     ['GetNewUniqueBlipIndex'] = {'int (__cdecl *)(int)', 0x582820},
     ['GetActualBlipArrayIndex'] = {'int (__cdecl *)(int)', 0x582870},
     ['LimitToMap'] = {'void (__cdecl *)(float*, float*)', 0x583350},
     ['CalculateBlipAlpha'] = {'const char* (__cdecl *)(float)', 0x583420},
-    ['TransformRealWorldPointToRadarSpace'] = {'void (__cdecl*)(struct CVector2D*, struct CVector2D*)', 0x583530},
     ['TransformRadarPointToScreenSpace'] = {'void (__cdecl*)(struct CVector2D*, struct CVector2D*)', 0x583480},
+    ['TransformRealWorldPointToRadarSpace'] = {'void (__cdecl*)(struct CVector2D*, struct CVector2D*)', 0x583530},
+    ['TransformRadarPointToRealWorldSpace'] = {'void (__cdecl *)(struct CVector2D*, struct CVector2D*)', 0x5835A0},
+    ['TransformRealWorldToTexCoordSpace'] = {'void (__cdecl *)(struct CVector2D*, struct CVector2D*, int, int)', 0x583600},
+    ['CalculateCachedSinCos'] = {'void (__cdecl *)()', 0x583670},
     ['IsPointInsideRadar'] = {'bool (__cdecl*)(struct CVector2D*)', 0x584D40}
-}
-return require('lua_sdk.loader')(functions)
+})
