e1b8cbd206d8a865d8d3bc18a8a58db3232df2be
Дима <82971106+GovnocodedByChapo@users.noreply.github.com>
2022-11-16 22:18:51 +0300

    Update loader.lua

--- lua_sdk/loader.lua
+++ lua_sdk/loader.lua
@@ -1,10 +1,17 @@
 local ffi = require('ffi')
-return function(functions)
+
+return function(functions, enums)
     local result = {}
     for Name, Data in pairs(functions) do
         _G['_'..Name] = ffi.cast(Data[1], Data[2])
         result[Name] = function(...)
             return _G['_'..Name](...)
+        end
+    end
+
+    if enums then
+        for k, v in pairs(enums) do
+            _G[k] = v
         end
     end
     return result;
